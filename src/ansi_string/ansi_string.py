e08ac9677ac65b845b087e2b4e977a447bb196cc
James Smith <jmsmith86@gmail.com>
2024-04-19 21:37:38 -0600

    Added functionality and tests

--- src/ansi_string/ansi_string.py
+++ src/ansi_string/ansi_string.py
@@ -826,8 +826,7 @@ class AnsiString:
 
             for i, item in enumerate(settings):
                 if isinstance(item, str):
-                    # Use string verbatim
-                    pass
+                    settings[i] = __class__._scrub_ansi_format_string(item)
                 elif hasattr(item, 'value') and isinstance(item.value, str):
                     # Likely an enumeration - use the value
                     settings[i] = item.value
@@ -835,6 +834,70 @@ class AnsiString:
                     raise TypeError('Unsupported type for setting_or_settings: {}'.format(type(setting_or_settings)))
 
             self._str = ';'.join(settings)
+
+        @staticmethod
+        def _parse_rgb_string(s:str):
+            component_dict = {
+                'ul_': ColorComponentType.UNDERLINE,
+                'bg_': ColorComponentType.BACKGROUND,
+                'fg_': ColorComponentType.FOREGROUND
+            }
+
+            # rgb(), fg_rgb(), bg_rgb(), or ul_rgb() with 3 distinct values as decimal or hex
+            match = re.search(r'^((?:fg_)?|(?:bg_)|(?:ul_))rgb\(\[?\s*(0x)?([0-9a-fA-F]+)\s*,\s*(0x)?([0-9a-fA-F]+)\s*,\s*(0x)?([0-9a-fA-F]+)\s*\]?\)$', s)
+            if match:
+                try:
+                    r = int(match.group(3), 16 if match.group(2) else 10)
+                    g = int(match.group(5), 16 if match.group(4) else 10)
+                    b = int(match.group(7), 16 if match.group(6) else 10)
+                except ValueError:
+                    raise ValueError('Invalid rgb value(s)')
+                return AnsiFormat.rgb(r, g, b, component_dict.get(match.group(1), ColorComponentType.FOREGROUND))
+
+            # rgb(), fg_rgb(), bg_rgb(), or ul_rgb() with 1 value as decimal or hex
+            match = re.search(r'^((?:fg_)?|(?:bg_)|(?:ul_))rgb\(\[?\s*(0x)?([0-9a-fA-F]+)\s*\]?\)$', s)
+            if match:
+                try:
+                    rgb = int(match.group(3), 16 if match.group(2) else 10)
+                except ValueError:
+                    raise ValueError('Invalid rgb value')
+                return AnsiFormat.rgb(rgb, component=component_dict.get(match.group(1), ColorComponentType.FOREGROUND))
+            return None
+
+        @staticmethod
+        def _scrub_ansi_format_string(ansi_format):
+            if ansi_format.startswith("["):
+                # Use the rest of the string as-is for settings
+                return ansi_format[1:]
+            else:
+                # The format string contains names within AnsiFormat or integers, separated by semicolon
+                formats = ansi_format.split(';')
+                format_settings_strs = []
+                for format in formats:
+                    ansi_fmt_enum = None
+                    try:
+                        ansi_fmt_enum = AnsiFormat[format.upper()]
+                    except KeyError:
+                        pass
+                    else:
+                        format_settings_strs.append(ansi_fmt_enum.value)
+
+                    if ansi_fmt_enum is None:
+                        rgb_format = __class__._parse_rgb_string(format)
+                        if not rgb_format:
+                            try:
+                                _ = int(format)
+                            except ValueError:
+                                raise ValueError(
+                                    'AnsiString.__format__ failed to parse format ({}); invalid name: {}'
+                                    .format(ansi_format, format)
+                                )
+                            else:
+                                # Value is an integer - use the format verbatim
+                                format_settings_strs.append(format)
+                        else:
+                            format_settings_strs.append(rgb_format)
+                return ';'.join(format_settings_strs)
 
         def __str__(self):
             return self._str
@@ -885,55 +948,42 @@ class AnsiString:
     def apply_formatting(
             self,
             setting_or_settings:Union[List[str], str, List[AnsiFormat], AnsiFormat],
-            start_idx_or_slice:Union[int, slice]=0,
-            length=None,
+            start:int=0,
+            end:Union[int,None]=None,
             topmost=True
     ):
         '''
         Sets the formatting for a given range of characters.
         Inputs: setting_or_settings - Can either be a single item or list of items;
                                       each item can either be a string or AnsiFormat type
-                start_idx_or_slice - If int, the string start index where setting(s) are to be applied
-                                     If slice, the slice specifying where to apply settings (length must not be set)
-                length - Number of characters to apply settings or None to apply until end of string
+                start - The string start index where setting(s) are to be applied
+                end - The string index where the setting(s) should be removed
                 topmost - When true, this setting is placed at the end of the set for the given
                         start_index meaning it is applied last; when false, setting is applied first
 
         Note: The desired effect may not be achieved if the same setting is applied over an
               overlapping range of characters.
         '''
-        if not setting_or_settings:
+        if not setting_or_settings or start >= len(self._s):
             # Ignore - nothing to apply
             return
+
+        if end is None:
+            length = None
+        else:
+            length = end - start
+            if length <= 0:
+                # Ignore - nothing to apply
+                return
 
         settings = __class__.Settings(setting_or_settings)
 
-        if isinstance(start_idx_or_slice, slice):
-            if length is not None:
-                raise ValueError('length cannot be set when start_idx_or_slice is a slice')
-            elif start_idx_or_slice.step is not None and start_idx_or_slice.step != 1:
-                raise ValueError('Step other than 1 not supported')
-
-            if start_idx_or_slice.start is None:
-                start_idx = 0
-            else:
-                start_idx = start_idx_or_slice.start
-
-            if start_idx_or_slice.stop is not None:
-                length = start_idx_or_slice.stop - start_idx
-        else:
-            start_idx = start_idx_or_slice
-
-        if length is not None and length <= 0:
-            # Ignore - nothing to apply
-            return
-
         # Apply settings
-        self._insert_settings(start_idx, True, settings, topmost)
+        self._insert_settings(start, True, settings, topmost)
 
         if length is not None:
             # Remove settings
-            self._insert_settings(start_idx + length, False, settings, topmost)
+            self._insert_settings(start + length, False, settings, topmost)
 
     def apply_formatting_for_match(
             self,
@@ -946,7 +996,7 @@ class AnsiString:
         '''
         s = match_object.start(group)
         e = match_object.end(group)
-        self.apply_formatting(setting_or_settings, s, e - s)
+        self.apply_formatting(setting_or_settings, s, e)
 
     def clear_formatting(self):
         '''
@@ -1039,35 +1089,6 @@ class AnsiString:
             result.append(match)
         return match
 
-    @staticmethod
-    def _parse_rgb_string(s:str):
-        component_dict = {
-            'ul_': ColorComponentType.UNDERLINE,
-            'bg_': ColorComponentType.BACKGROUND,
-            'fg_': ColorComponentType.FOREGROUND
-        }
-
-        # rgb(), fg_rgb(), bg_rgb(), or ul_rgb() with 3 distinct values as decimal or hex
-        match = re.search(r'^((?:fg_)?|(?:bg_)|(?:ul_))rgb\(\s*(0x)?([0-9a-fA-F]+)\s*,\s*(0x)?([0-9a-fA-F]+)\s*,\s*(0x)?([0-9a-fA-F]+)\s*\)$', s)
-        if match:
-            try:
-                r = int(match.group(3), 16 if match.group(2) else 10)
-                g = int(match.group(5), 16 if match.group(4) else 10)
-                b = int(match.group(7), 16 if match.group(6) else 10)
-            except ValueError:
-                raise ValueError('Invalid rgb value(s)')
-            return AnsiFormat.rgb(r, g, b, component_dict.get(match.group(1), ColorComponentType.FOREGROUND))
-
-        # rgb(), fg_rgb(), bg_rgb(), or ul_rgb() with 1 value as decimal or hex
-        match = re.search(r'^((?:fg_)?|(?:bg_)|(?:ul_))rgb\(\s*(0x)?([0-9a-fA-F]+)\s*\)$', s)
-        if match:
-            try:
-                rgb = int(match.group(3), 16 if match.group(2) else 10)
-            except ValueError:
-                raise ValueError('Invalid rgb value')
-            return AnsiFormat.rgb(rgb, component=component_dict.get(match.group(1), ColorComponentType.FOREGROUND))
-        return None
-
     def __format__(self, __format_spec:str) -> str:
         '''
         Returns an ANSI format string with both internal and given formatting spec set.
@@ -1142,39 +1163,7 @@ class AnsiString:
                 raise ValueError('Invalid format specifier')
 
         if ansi_format:
-            if ansi_format.startswith("["):
-                # Use the rest of the string as-is for settings
-                format_settings = __class__.Settings(ansi_format[1:])
-            else:
-                # The format string contains names within AnsiFormat or integers, separated by semicolon
-                formats = ansi_format.split(';')
-                format_settings_strs = []
-                for format in formats:
-                    ansi_fmt_enum = None
-                    try:
-                        ansi_fmt_enum = AnsiFormat[format.upper()]
-                    except KeyError:
-                        pass
-                    else:
-                        format_settings_strs.append(ansi_fmt_enum.value)
-
-                    if ansi_fmt_enum is None:
-                        rgb_format = __class__._parse_rgb_string(format)
-                        if not rgb_format:
-                            try:
-                                _ = int(format)
-                            except ValueError:
-                                raise ValueError(
-                                    'AnsiString.__format__ failed to parse format ({}); invalid name: {}'
-                                    .format(ansi_format, format)
-                                )
-                            else:
-                                # Value is an integer - use the format verbatim
-                                format_settings_strs.append(format)
-                        else:
-                            format_settings_strs.append(rgb_format)
-                format_settings = __class__.Settings(';'.join(format_settings_strs))
-
+            format_settings =  __class__.Settings(ansi_format)
             __class__._insert_settings_to_dict(settings_dict, 0, True, format_settings, True)
 
         clear_needed = False
@@ -1203,3 +1192,114 @@ class AnsiString:
             out_str += __class__.ANSI_ESCAPE_CLEAR
 
         return out_str
+
+    def capitalize(self):
+        cpy = copy.deepcopy(self)
+        cpy._s = cpy._s.capitalize()
+        return cpy
+
+    def casefold(self):
+        cpy = copy.deepcopy(self)
+        cpy._s = cpy._s.casefold()
+        return cpy
+
+    def center(self, width:int, fillchar:str=' '):
+        cpy = copy.deepcopy(self)
+        num = width - len(cpy._s)
+        if num > 0:
+            left_spaces = math.floor((num) / 2)
+            right_spaces = num - left_spaces
+            cpy._s = fillchar * left_spaces + cpy._s + fillchar * right_spaces
+            # Shift all indices except for the origin
+            __class__._shift_settings_idx(cpy._color_settings, left_spaces, True)
+        return cpy
+
+    def count(self, sub:str, start:int, end:int) -> int:
+        return self._s.count(sub, start, end)
+
+    def encode(self, encoding:str="utf-8", errors:str="strict") -> bytes:
+        return str(self).encode(encoding, errors)
+
+    def endswith(self, suffix:str, start:int=None, end:int=None) -> bool:
+        return self._s.endswith(suffix, start, end)
+
+    def expandtabs(self, tabsize:int=8):
+        cpy = copy.deepcopy(self)
+        cpy._s = cpy._s.expandtabs(tabsize)
+        return cpy
+
+    def find(self, sub:str, start:int=None, end:int=None) -> int:
+        return self._s.find(sub, start, end)
+
+    def index(self, sub:str, start:int=None, end:int=None) -> int:
+        return self._s.index(sub, start, end)
+
+    def isalnum(self) -> bool:
+        return self._s.isalnum()
+
+    def isalpha(self) -> bool:
+        return self._s.isalpha()
+
+    def isascii(self) -> bool:
+        return self._s.isascii()
+
+    def isdecimal(self) -> bool:
+        return self._s.isdecimal()
+
+    def isdigit(self) -> bool:
+        return self._s.isdigit()
+
+    def isidentifier(self) -> bool:
+        return self._s.isidentifier()
+
+    def islower(self) -> bool:
+        return self._s.islower()
+
+    def isnumeric(self) -> bool:
+        return self._s.isnumeric()
+
+    def isprintable(self) -> bool:
+        return self._s.isprintable()
+
+    def isspace(self) -> bool:
+        return self._s.isspace()
+
+    def istitle(self) -> bool:
+        return self._s.istitle()
+
+    def isupper(self) -> bool:
+        return self._s.isupper()
+
+    def __add__(self, value):
+        cpy = copy.deepcopy(self)
+        cpy += value
+        return cpy
+
+    def __iadd__(self, value):
+        if isinstance(value, str):
+            self._s += value
+        elif isinstance(value, AnsiString):
+            settings_cpy = copy.deepcopy(value._color_settings)
+            __class__._shift_settings_idx(settings_cpy, len(self._s), False)
+            self._s += value._s
+            for key, value in settings_cpy.values():
+                self._color_settings[key] = value
+        else:
+            raise ValueError(f'value is invalid type: {type(value)}')
+        return self
+
+    @staticmethod
+    def join(*args):
+        if not args:
+            return AnsiString()
+        args = list(args)
+        first_arg = args[0]
+        if isinstance(first_arg, str):
+            joint = AnsiString(first_arg)
+        elif isinstance(first_arg, AnsiString):
+            joint = copy.deepcopy(first_arg)
+        else:
+            raise ValueError(f'value is invalid type: {type(first_arg)}')
+        for arg in args[1:]:
+            joint += arg
+        return joint
--- tests/test_ansi_string.py
+++ tests/test_ansi_string.py
@@ -5,7 +5,14 @@ import sys
 import unittest
 from io import BytesIO, StringIO
 from unittest.mock import patch
-import ansi_string
+
+THIS_FILE_PATH = os.path.dirname(os.path.abspath(os.path.realpath(__file__)))
+PROJECT_DIR = os.path.abspath(os.path.join(THIS_FILE_PATH, '..'))
+SOURCE_DIR = os.path.abspath(os.path.join(PROJECT_DIR, 'src'))
+
+if os.path.isdir(SOURCE_DIR):
+    sys.path.insert(0, SOURCE_DIR)
+from ansi_string import en_tty_ansi, AnsiFormat, AnsiString, ColorComponentType, ColourComponentType
 
 def _is_windows():
     return sys.platform.lower().startswith('win')
@@ -21,8 +28,78 @@ class FakeStdIn:
         self.buffer = BytesIO(loaded_str)
 
 class CliTests(unittest.TestCase):
-    def test_placeholder(self):
-        self.assertTrue(True)
+    def test_en_tty_ansi(self):
+        # Not a very useful test
+        en_tty_ansi()
+
+    def test_no_format(self):
+        s = AnsiString('No format')
+        self.assertEqual(str(s), 'No format')
+
+    def test_using_AnsiFormat(self):
+        s = AnsiString('This is bold', AnsiFormat.BOLD)
+        self.assertEqual(str(s), '\x1b[1mThis is bold\x1b[m')
+
+    def test_using_list_of_AnsiFormat(self):
+        s = AnsiString('This is bold and red', [AnsiFormat.BOLD, AnsiFormat.RED])
+        self.assertEqual(str(s), '\x1b[1;31mThis is bold and red\x1b[m')
+
+    def test_using_list_of_various(self):
+        s = AnsiString('Lots of formatting!', ['[1', AnsiFormat.UL_RED, 'rgb(0x12A03F);bg_white'])
+        self.assertEqual(str(s), '\x1b[1;4;58;5;9;38;2;18;160;63;47mLots of formatting!\x1b[m')
+
+    def test_custom_formatting(self):
+        s = AnsiString('This string contains custom formatting', '[38;2;175;95;95')
+        self.assertEqual(str(s), '\x1b[38;2;175;95;95mThis string contains custom formatting\x1b[m')
+
+    def test_ranges(self):
+        s = AnsiString('This string contains multiple color settings across different ranges')
+        s.apply_formatting(AnsiFormat.BOLD, 5, 11)
+        s.apply_formatting(AnsiFormat.BG_BLUE, 21, 29)
+        s.apply_formatting([AnsiFormat.FG_ORANGE, AnsiFormat.ITALIC], 21, 35)
+        self.assertEqual(
+            str(s),
+            'This \x1b[1mstring\x1b[m contains \x1b[44;38;5;214;3mmultiple\x1b[0;38;5;214;3m color\x1b[m settings '
+            'across different ranges'
+        )
+
+    def test_format_right_justify_and_int(self):
+        s = AnsiString('This string will be formatted bold and red, right justify')
+        self.assertEqual(
+            f'{s:>90:01;31}',
+            '\x1b[01;31m                                 This string will be formatted bold and red, right justify\x1b[m'
+        )
+
+    def test_format_left_justify_and_strings(self):
+        s = AnsiString('This string will be formatted bold and red')
+        self.assertEqual(
+            '{:+<90:bold;fg_red}'.format(s),
+            '\x1b[1;31mThis string will be formatted bold and red++++++++++++++++++++++++++++++++++++++++++++++++\x1b[m'
+        )
+
+    def test_format_center_and_verbatim_string(self):
+        s = AnsiString('This string will be formatted bold and red')
+        self.assertEqual(
+            '{:*^90:[this is not parsed}'.format(s),
+            '\x1b[this is not parsedm************************This string will be formatted bold and red************************\x1b[m'
+        )
+
+    def test_no_format_and_rgb_functions(self):
+        s = AnsiString('Manually adjust colors of foreground, background, and underline')
+        self.assertEqual(
+            f'{s::rgb(0x8A2BE2);bg_rgb(100, 232, 170);ul_rgb(0xFF, 0x63, 0x47)}',
+            '\x1b[38;2;138;43;226;48;2;100;232;170;4;58;2;255;99;71mManually adjust colors of foreground, background, and underline\x1b[m'
+        )
+
+    def test_no_format_and_rgb_functions2(self):
+        s = AnsiString('Manually adjust colors of foreground, background, and underline')
+        fg_color = 0x8A2BE2
+        bg_colors = [100, 232, 170]
+        ul_colors = [0xFF, 0x63, 0x47]
+        self.assertEqual(
+            f'{s::rgb({fg_color});bg_rgb({bg_colors});ul_rgb({ul_colors})}',
+            '\x1b[38;2;138;43;226;48;2;100;232;170;4;58;2;255;99;71mManually adjust colors of foreground, background, and underline\x1b[m'
+        )
 
 if __name__ == '__main__':
     unittest.main()
