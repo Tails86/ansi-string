685f26c215493bb8fc6a2d34d31427bcfdc732be
James Smith <jmsmith86@gmail.com>
2024-04-19 18:54:03 -0600

    Bumped version 0.1.0

--- src/ansi_string/ansi_string.py
+++ src/ansi_string/ansi_string.py
@@ -28,7 +28,7 @@ from enum import Enum, EnumMeta, auto as enum_auto
 import io
 from typing import Any, Union, List
 
-__version__ = '0.0.4'
+__version__ = '0.1.0'
 PACKAGE_NAME = 'ansi-string'
 
 IS_WINDOWS = sys.platform.lower().startswith('win')
